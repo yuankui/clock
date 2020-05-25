a0adc61102d7045487bfef68e8145640ef21880f
yuankui <yuankui64@gmail.com>
2020-05-25 23:04:39 +0800

    up

---
+++ src/clock/Clock.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+interface Props {}
+
+const Clock: FunctionComponent<Props> = (props) => {
+    return <div className='clock'>
+        11
+    </div>;
+};
+
+export default Clock;
